98aceac1b0fdf47f00d1ce662551621f2096f7b7
mpondev <mario@mpondev.com>
2024-01-19 09:26:30 +0100

    Add CalContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import CalContainer from './components/CalContainer/CalContainer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="app">
       <div>Calendar</div>
+      <CalContainer />
     </div>
   );
 }
---
+++ src/components/CalContainer/CalContainer.jsx
@@ -0,0 +1,7 @@
+import './CalContainer.scss';
+
+function CalContainer() {
+  return <div className="cal-container">CalContainer</div>;
+}
+
+export default CalContainer;
