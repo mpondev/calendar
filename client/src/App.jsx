44cd1456eef11a48a9ff2a0946602009f98529f5
Mario Ponce <mario@mpondev.com>
2024-01-24 18:45:27 +0100

    Add react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import CalContainer from './components/CalContainer/CalContainer';
 
 import './App.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <CalContainer />,
+  },
+]);
+
 function App() {
   return (
     <div className="app">
-      <CalContainer />
+      <RouterProvider router={router} />
     </div>
   );
 }
