1217bce7a2a530b5eb93e5876d40d659c199a8bd
mpondev <mario@mpondev.com>
2024-06-14 12:05:18 +0200

    Add basic client and server structure

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import styles from './styles/App.module.css';
+
+function App(): JSX.Element {
+  return <div className={styles.app}>Calendar</div>;
+}
+
+export default App;
---
+++ server/src/server.ts
@@ -0,0 +1,6 @@
+import app from './index';
+
+const PORT = 3010;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}...`);
+});
