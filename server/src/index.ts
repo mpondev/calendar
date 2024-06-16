5a356a4df835993058dab5d00a8b85173028dbfd
Mario Ponce <mario@mpondev.com>
2024-06-16 12:35:23 +0200

    Add tasks.json to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
-import express from 'express';
+import express, { Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (_, res) => {
+app.get('/', (_, res: Response) => {
   res.send('Hello World!');
 });
 
