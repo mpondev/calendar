d4665dbb94192872e1f7d8bdb60671d05bc44034
Mario Ponce <mario@mpondev.com>
2024-06-16 11:04:21 +0200

    Add DailyCell component and MonthGrid functionality

---
+++ client/src/components/DailyCell/DailyCell.tsx
@@ -0,0 +1,39 @@
+import dayjs from 'dayjs';
+import { useDateStore } from '../../store/dateStore';
+
+import styles from './DailyCell.module.css';
+
+interface Props {
+  day: dayjs.Dayjs;
+}
+
+function DailyCell({ day }: Props): JSX.Element {
+  const { date } = useDateStore();
+
+  const isCurrentDay = day.format('DD-MM-YY') === dayjs().format('DD-MM-YY');
+  const isPrevMonth = day <= dayjs(date).date(0);
+  const isNextMonth = day > dayjs(date).date(date.daysInMonth());
+
+  return (
+    <td
+      className={
+        isCurrentDay
+          ? `${styles.dailyCell} ${styles.currentDay}`
+          : `${styles.dailyCell}`
+      }
+    >
+      <div className={styles.cellWrapper}>
+        <header className={styles.top}>
+          <p
+            className={isPrevMonth || isNextMonth ? `${styles.otherMonth}` : ''}
+          >
+            {day.format('D')}
+          </p>
+        </header>
+        <div className={styles.body}></div>
+      </div>
+    </td>
+  );
+}
+
+export default DailyCell;
--- client/src/components/MonthGrid/MonthGrid.tsx
+++ client/src/components/MonthGrid/MonthGrid.tsx
@@ -1,7 +1,57 @@
+import dayjs from 'dayjs';
+import { useDateStore } from '../../store/dateStore';
+
+import DailyCell from '../DailyCell/DailyCell';
+
 import styles from './MonthGrid.module.css';
 
 function MonthGrid(): JSX.Element {
-  return <div className={styles.grid}>MonthGrid</div>;
+  const { date } = useDateStore();
+
+  const year: number = date.year();
+  const month: number = date.month();
+  const firstDayMonth: 0 | 1 | 2 | 3 | 4 | 5 | 6 = date.date(1).day();
+  let currentMonthCount: number = 0 - firstDayMonth;
+
+  const monthGrid: dayjs.Dayjs[][] = new Array(6).fill([]).map(() => {
+    return new Array(7).fill(null).map(() => {
+      currentMonthCount++;
+      return dayjs(new Date(year, month, currentMonthCount));
+    });
+  });
+
+  const weekdays: string[] = new Array(7).fill(null).map((_, i) => {
+    return dayjs().day(i).format('ddd');
+  });
+
+  return (
+    <div className={styles.grid}>
+      <table className={styles.table}>
+        <thead className={styles.head}>
+          <tr>
+            {weekdays.map((day, i) => (
+              <th key={i}>
+                <span>{day}</span>
+              </th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {monthGrid.map((row, i) => (
+            <tr key={i}>
+              {row.map(day => (
+                <DailyCell
+                  day={day}
+                  key={`${day.year()}${day.month()}${day.date()}`}
+                />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default MonthGrid;
