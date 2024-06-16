000b3898345d4fc7c49f16e2ac3b28c0521956aa
Mario Ponce <mario@mpondev.com>
2024-06-16 09:57:54 +0200

    Add react-icons and compose CalHeader

--- client/src/components/CalHeader/CalHeader.tsx
+++ client/src/components/CalHeader/CalHeader.tsx
@@ -1,7 +1,38 @@
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa6';
+
 import styles from './CalHeader.module.css';
 
 function CalHeader(): JSX.Element {
-  return <div className={styles.header}>CalHeader</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.moveTo}>
+        <div className={styles.btnGroup}>
+          <button title="Previous month">
+            <FaAngleLeft className={styles.fa} />
+          </button>
+          <button title="Next month">
+            <FaAngleRight className={styles.fa} />
+          </button>
+        </div>
+        <button title="Today" className={styles.today}>
+          today
+        </button>
+      </div>
+
+      <div className={styles.date}>
+        <h2>April 2024</h2>
+      </div>
+
+      <div>
+        <div className={styles.btnGroup}>
+          <button title="Month view">month</button>
+          <button title="Week view">week</button>
+          <button title="Day view">day</button>
+          <button title="List view">list</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CalHeader;
