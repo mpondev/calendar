fdee9a3c0fd73698a18eac91e117e191d7b53474
Mario Ponce <mario@mpondev.com>
2025-02-24 10:47:14 +0100

    Style ListGrid component

--- client/src/routes/ListGrid.jsx
+++ client/src/routes/ListGrid.jsx
@@ -1,5 +1,23 @@
+import { useDateStore } from '../store/dateStore';
+
 const ListGrid = () => {
-  return <div className="flex-1">ListGrid</div>;
+  const { date } = useDateStore();
+
+  return (
+    <div className="flex-1 overflow-y-auto shadow-sm">
+      <table className="h-full w-full table-fixed border-separate border-spacing-0 border-1 border-gray-300">
+        <thead className="sticky top-0 bg-slate-100">
+          <tr className="h-8">
+            <th className="flex justify-between border-1 border-gray-300 px-4 py-2 font-medium">
+              <span>{date.format('dddd')}</span>
+              <span>{date.format('D-MMM-YYYY')}</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ListGrid;
--- client/src/routes/WeekGrid.jsx
+++ client/src/routes/WeekGrid.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import dayjs from 'dayjs';
 
 import { useDateStore } from '../store/dateStore';
 
